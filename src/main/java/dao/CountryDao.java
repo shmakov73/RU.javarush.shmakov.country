de7bcc72ba7dba38c39de9e6f3ebee7ac11fed63
shmakov73 <shmakov73@gmail.com>
2023-02-09 16:50:56 +0400

    added dao package with Dao classes

---
+++ src/main/java/dao/CountryDao.java
@@ -0,0 +1,22 @@
+package dao;
+
+import domain.Country;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class CountryDao {
+    private final SessionFactory sessionFactory;
+
+    public CountryDao(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public List<Country> getAll() {
+        Query<Country> query = sessionFactory
+                .getCurrentSession()
+                .createQuery("select c from Country c join fetch c.languages", Country.class);
+        return query.list();
+    }
+}
